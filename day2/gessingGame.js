fd6324b8e8d949ea2e81548da5d673185d786455
Steve <stevebowling@SteveBowlingMBP.local>
2016-09-20 12:07:19 -0400

    lunch

---
+++ day2/gessingGame.js
@@ -0,0 +1,41 @@
+
+var answer;
+
+function setupGame() {
+
+    $("#tbxGuess").val("");
+    $("#message").val("");
+
+    answer = parseInt(Math.random() * 10);
+    console.log("Cheater!!" + answer);
+
+    $("#intro").show();
+    $("#game").hide();
+}
+
+function handleGuess() {
+    var guess = $("#tbxGuess").val();
+    if (+guess == answer) {
+        $("#message").text("you are right");
+
+    }
+    else {
+        $("#message").text("Try again");
+        $("#tbxGuess").val("");
+        $("#tbxGuess").focus();
+    }
+}
+
+function playGame() {
+    $("#intro").hide();
+    $("#game").show();
+}
+$(function () {
+    $("#startGame").on("click", playGame);
+    $("#btnGuess").on("click", handleGuess);
+    setupGame();
+
+});
+function startOver() {
+    location.reload();
+}
