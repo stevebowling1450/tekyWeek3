d74544f54709c072423ec47969c51564286ecbf3
Steve <stevebowling@SteveBowlingMBP.local>
2016-09-20 09:41:39 -0400

    selector Challange

---
+++ day2/selectorChallenge.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#p1").mouseenter(function(){
+        $("#p1").fadeTo( "fast",0.5)
+    });
+});
+
+$(document).ready(function(){
+    $("h3").mouseenter(function(){
+        $("h3").fadeTo( "fast",0.5)
+    });
+});
+$(document).ready(function(){
+    $("ul :first-child").mouseenter(function(){
+        $("ul :first-child").fadeTo( "fast",0.5)
+    });
+});
